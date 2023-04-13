cc51349b9a1452580ffaf2d920815b6a3213d406
Karlos1235 <karol.novy123@seznam.cz>
2023-04-13 17:40:30 +0200

    added main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:skolappka/ui/screens/debug_screen.dart';
-import 'package:skolappka/ui/screens/home_screen.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,25 +10,29 @@ void main() async {
       path: 'assets/localization',
       supportedLocales: const [Locale('cs')],
       fallbackLocale: const Locale('cs'),
-      child: const MyApp()));
+      child:  MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Školappka',
-      initialRoute: '/',
-      routes: {
-        '/debug': (context) => const DebugScreen(),
-        '/home': (context) => const HomeScreen(),
-      },
+      title: 'SPSSECB Aktuality',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const DebugScreen(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Dukla'),
+        ),
+        body: const Padding(
+          padding: EdgeInsets.all(10.0),
+          child: WebView(
+            initialUrl: 'https://www.spssecb.cz/aktuality',
+            javascriptMode: JavascriptMode.unrestricted,
+          ),
+        ),
+      ),
     );
   }
 }
